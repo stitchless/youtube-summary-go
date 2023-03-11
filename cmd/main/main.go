fb0ebf8c30cbb03a49ab6689b49247eb2917f2bf
Josh Peizer <contact@sent.sh>
2023-03-10 21:39:08 -0500

    Proof of concept

---
+++ cmd/main/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+
+	speech "cloud.google.com/go/speech/apiv1"
+	"cloud.google.com/go/speech/apiv1/speechpb"
+	"cloud.google.com/go/storage"
+	"github.com/kkdai/youtube/v2"
+	"github.com/sashabaranov/go-openai"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	VideoURL := "https://www.youtube.com/watch?v=XmD1bUMC4lk"
+
+	client := youtube.Client{}
+
+	video, err := client.GetVideo(VideoURL)
+	fmt.Println("Getting video: ", video.Title)
+	if err != nil {
+		panic(err)
+	}
+
+	formats := video.Formats.AudioChannels(2)
+	formats.FindByQuality("tiny")
+
+	if len(formats) == 0 {
+		panic("No video formats found")
+	}
+
+	for _, format := range formats {
+		fmt.Printf("%+v", format)
+	}
+
+	fmt.Println("Downloading video: ", video.Title)
+	stream, _, err := client.GetStream(video, &formats[0])
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Saving video: ", video.Title)
+	file, err := os.Create(video.Title + ".mp4")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		panic(err)
+	}
+
+	// Extract the audio from the video using ffmpeg.
+	audioPath := "audio.wav"
+	fmt.Println("Extracting audio: ", audioPath)
+	cmd := exec.Command("ffmpeg", "-i", file.Name(), "-vn", "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1", audioPath)
+	err = cmd.Run()
+	if err != nil {
+		log.Fatalf("Error extracting audio: %v", err)
+	}
+	defer os.Remove(audioPath)
+
+	ctx := context.Background()
+	homeDirectory, _ := os.UserHomeDir()
+	credFile := homeDirectory + "/.google/credentials.json"
+
+	// Upload the audio file to Google Cloud Storage.
+	storeClient, err := storage.NewClient(ctx, option.WithCredentialsFile(credFile))
+	if err != nil {
+		log.Fatalf("Error creating client: %v", err)
+	}
+	defer storeClient.Close()
+
+	bucketName := "yt_summary"
+	objectName := "audio.wav"
+
+	bucket := storeClient.Bucket(bucketName)
+	wc := bucket.Object(objectName).NewWriter(ctx)
+	defer wc.Close()
+
+	data, err := os.ReadFile(audioPath)
+	if err != nil {
+		log.Fatalf("Error reading audio file: %v", err)
+	}
+
+	fmt.Println("Uploading audio file to bucket: ", bucketName)
+	if _, err := wc.Write(data); err != nil {
+		log.Fatalf("Error writing audio file to bucket: %v", err)
+	}
+
+	if err := wc.Close(); err != nil {
+		log.Fatalf("Error closing bucket writer: %v", err)
+	}
+
+	// Transcribe the audio using the Google Cloud Speech-to-Text API.
+	speechClient, err := speech.NewClient(ctx, option.WithCredentialsFile(credFile))
+	if err != nil {
+		log.Fatalf("Error creating client: %v", err)
+	}
+	defer speechClient.Close()
+
+	fmt.Println("Transcribing audio file: ", objectName)
+	resp, err := speechClient.LongRunningRecognize(ctx, &speechpb.LongRunningRecognizeRequest{
+		Config: &speechpb.RecognitionConfig{
+			Encoding:        speechpb.RecognitionConfig_LINEAR16,
+			LanguageCode:    "en-US",
+			SampleRateHertz: 16000,
+		},
+		Audio: &speechpb.RecognitionAudio{
+			AudioSource: &speechpb.RecognitionAudio_Uri{Uri: fmt.Sprintf("gs://%s/%s", bucketName, objectName)},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error transcribing audio: %v", err)
+	}
+
+	wait, err := resp.Wait(ctx)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Transcription results:")
+	results := wait.GetResults()
+	transcript := ""
+	for _, result := range results {
+		alternatives := result.GetAlternatives()
+		for _, alternative := range alternatives {
+			fmt.Println(alternative.GetTranscript())
+			transcript += alternative.GetTranscript()
+		}
+	}
+
+	// OpenAI GPT-3
+	apiKey := os.Getenv("OPENAI_API_KEY")
+
+	prompt := fmt.Sprintf("Using this youtube video transcript: (video title: %s), generate an engaging summary of the video.  Be sure to remove anything that resembles an ad that may be contained within the transcript.\n\nTranscript:\n%s", video.Title, transcript)
+
+	fmt.Println("Generating summary: ", transcript)
+	openAIClient := openai.NewClient(apiKey)
+	aiResp, err := openAIClient.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo0301,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleAssistant,
+					Content: prompt,
+				},
+			},
+		},
+	)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("Summary:")
+	fmt.Println(aiResp.Choices[0].Message.Content)
+}
---
+++ go.mod
@@ -0,0 +1,39 @@
+module github.com/jpeizer/youtube-summary-go
+
+go 1.20
+
+require (
+	cloud.google.com/go/speech v1.14.1
+	cloud.google.com/go/storage v1.29.0
+	github.com/kkdai/youtube/v2 v2.7.18
+	google.golang.org/api v0.112.0
+)
+
+require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v0.12.0 // indirect
+	cloud.google.com/go/longrunning v0.4.1 // indirect
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/dlclark/regexp2 v1.7.0 // indirect
+	github.com/dop251/goja v0.0.0-20221115122301-6c0d9883792e // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/sashabaranov/go-openai v1.5.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
